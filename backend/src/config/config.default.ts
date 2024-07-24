813b480de52158481b37e63147dfde51048eab06
Assass1nHeart <ah2965273512@163.com>
2024-07-24 17:27:59 +0800

    删除
    删除

--- backend/src/config/config.default.ts
+++
@@ -1,9 +0,0 @@
-import { MidwayConfig } from '@midwayjs/core';
-
-export default {
-  // use for cookie sign key, should change to your own and keep security
-  keys: '1721184353478_4110',
-  koa: {
-    port: 7001,
-  },
-} as MidwayConfig;
