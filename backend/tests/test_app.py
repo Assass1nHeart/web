237d706b68bade58cdd6ee2fbe88098eca3f107d
Assass1nHeart <ah2965273512@163.com>
2024-07-25 10:50:37 +0800

    1
    1

---
+++ backend/tests/test_app.py
@@ -0,0 +1,11 @@
+# backend/tests/test_main.py
+
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "message" in response.json()
